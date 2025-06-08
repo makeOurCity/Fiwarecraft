e8573fc3d55176ccd27e023085ed0c834f4698d0
Shugo Ushio <ushio.s@gmail.com>
2025-06-08 21:46:36 +0900

    update dependencies config

--- src/main/java/city/makeour/fiwarecraft/App.java
+++ src/main/java/city/makeour/fiwarecraft/App.java
@@ -2,10 +2,16 @@ package city.makeour.fiwarecraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 /**
  * Fiwarecraft plugin main class
  */
 public class App extends JavaPlugin {
+
+  App() {
+    super();
+  }
+
   @Override
   public void onEnable() {
     getLogger().info("Fiwarecraft plugin has been enabled!");

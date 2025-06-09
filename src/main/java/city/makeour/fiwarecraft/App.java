6fa0a359d05b954128e8ed990e7500a0b04e4b81
Shugo Ushio <ushio.s@gmail.com>
2025-06-09 11:26:26 +0900

    Add test

--- src/main/java/city/makeour/fiwarecraft/App.java
+++ src/main/java/city/makeour/fiwarecraft/App.java
@@ -2,19 +2,35 @@ package city.makeour.fiwarecraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import city.makeour.moc.MocClient;
+
 
 /**
  * Fiwarecraft plugin main class
  */
 public class App extends JavaPlugin {
 
-  App() {
+  protected MocClient mocClient;
+
+  /**
+   * デフォルトのコンストラクタ
+   */
+  public App() {
     super();
+  }
+
+  /**
+   * MockBukkitのテスト用コンストラクタ
+   */
+  protected App(org.bukkit.plugin.java.JavaPluginLoader loader, org.bukkit.plugin.PluginDescriptionFile description,
+          java.io.File dataFolder, java.io.File file) {
+    super(loader, description, dataFolder, file);
   }
 
   @Override
   public void onEnable() {
     getLogger().info("Fiwarecraft plugin has been enabled!");
+    this.mocClient = new MocClient();
   }
 
   @Override
--- src/test/java/city/makeour/fiwarecraft/AppTest.java
+++ src/test/java/city/makeour/fiwarecraft/AppTest.java
@@ -1,18 +1,47 @@
 package city.makeour.fiwarecraft;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+
 /**
- * Unit test for simple App.
+ * Unit test for App plugin.
  */
 public class AppTest {
-  /**
-   * Rigorous Test :-)
-   */
+
+  @SuppressWarnings("unused")
+  private ServerMock server;
+  private App plugin;
+
+  @Before
+  public void setUp() {
+    // MockBukkitサーバーを起動
+    server = MockBukkit.mock();
+    // プラグインをロード
+    plugin = MockBukkit.load(App.class);
+  }
+
+  @After
+  public void tearDown() {
+    // MockBukkitサーバーをシャットダウン
+    MockBukkit.unmock();
+  }
+
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  public void testPluginEnabled() {
+    // プラグインが正しくロードされていることを確認
+    assertTrue(plugin.isEnabled());
+  }
+
+  @Test
+  public void testMocClientInitialized() {
+    // MocClientが正しく初期化されていることを確認
+    assertNotNull(plugin.mocClient);
   }
 }

afe22b558b3bdd37a33dafbdb4ae1e53239a4ff3
Shugo Ushio <ushio.s@gmail.com>
2025-06-16 12:13:25 +0900

    update json entity

--- src/main/java/city/makeour/fiwarecraft/model/Ping.java
+++ src/main/java/city/makeour/fiwarecraft/model/Ping.java
@@ -4,13 +4,29 @@ import javax.json.bind.annotation.JsonbProperty;
 
 public class Ping extends NgsiV2Entity {
 
-    @JsonbProperty("timestamp")
-    public String getTimestamp() {
-        return String.valueOf(System.currentTimeMillis());
+    public static final String JSON_PROPERTY_LAST_SUCCEEDED_AT = "lastSucceededAt";
+    private String lastSucceededAt;
+
+    public static final String JSON_PROPERTY_STATUS = "status";
+    private boolean status;
+
+    @JsonbProperty(JSON_PROPERTY_LAST_SUCCEEDED_AT)
+    public void setLastSucceededAt(String lastSucceededAt) {
+        this.lastSucceededAt = lastSucceededAt;
     }
 
-    @JsonbProperty("status")
+    @JsonbProperty(JSON_PROPERTY_LAST_SUCCEEDED_AT)
+    public String getLastSucceededAt() {
+        return this.lastSucceededAt;
+    }
+
+    @JsonbProperty(JSON_PROPERTY_STATUS)
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @JsonbProperty(JSON_PROPERTY_STATUS)
     public String getStatus() {
-        return "ok";
+        return this.status ? "OK" : "NG";
     }
 }

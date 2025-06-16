9996d7c1c0fab535e1a7d5d24c133ed075fbe670
Shugo Ushio <ushio.s@gmail.com>
2025-06-16 13:33:12 +0900

    add ping method

--- src/main/java/city/makeour/fiwarecraft/client/FcMocClient.java
+++ src/main/java/city/makeour/fiwarecraft/client/FcMocClient.java
@@ -1,13 +1,55 @@
 package city.makeour.fiwarecraft.client;
 
 import city.makeour.moc.MocClient;
+import city.makeour.fiwarecraft.model.Ping;
+import java.time.LocalDateTime;
 
 public class FcMocClient {
 
     protected MocClient mocClient;
+    private String fiwareService;
 
     public FcMocClient(MocClient mocClient) {
         this.mocClient = mocClient;
+        this.fiwareService = "fiwarecraft";
     }
 
+    public FcMocClient(MocClient mocClient, String fiwareService) {
+        this.mocClient = mocClient;
+        this.fiwareService = fiwareService;
+    }
+
+    public void sendPing(String entityId, boolean status) {
+        Ping pingEntity = new Ping();
+        pingEntity.setType("Ping");
+        pingEntity.setId(entityId);
+        pingEntity.setLastSucceededAt(LocalDateTime.now());
+        pingEntity.setStatus(status);
+
+        sendPing(pingEntity);
+    }
+
+    public void sendPing(Ping pingEntity) {
+        if (pingEntity.getType() == null) {
+            pingEntity.setType("Ping");
+        }
+
+        if (pingEntity.getLastSucceededAt() == null) {
+            pingEntity.setLastSucceededAt(LocalDateTime.now());
+        }
+
+        if (this.fiwareService != null && !this.fiwareService.isEmpty()) {
+            mocClient.setFiwareService(this.fiwareService);
+        }
+
+        mocClient.createEntity("application/json", pingEntity);
+    }
+
+    public void setFiwareService(String fiwareService) {
+        this.fiwareService = fiwareService;
+    }
+
+    public String getFiwareService() {
+        return this.fiwareService;
+    }
 }
